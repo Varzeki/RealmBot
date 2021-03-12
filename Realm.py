57bd3e797ee3fe68c524e151154dad5498c0d937
varzeki <expectthespanish@gmail.com>
2021-03-13 00:47:58 +1100

    Added performance timings

--- Realm.py
+++ Realm.py
@@ -1154,14 +1154,26 @@ async def on_ready():
     print("Channel Initialization Complete")
     print("Commencing Cycle")
     while not graceful_exit:
+        tic = time.perf_counter()
         try:
             await doCombat()
         except:
             print("Error during combat routine")
             print(traceback.format_exc())
+        toc = time.perf_counter()
+        print(f"Combat complete in {toc - tic:0.4f} seconds")
+        tic = time.perf_counter()
         await doHealthRegen()
+        toc = time.perf_counter()
+        print(f"Health regen complete in {toc - tic:0.4f} seconds")
+        tic = time.perf_counter()
         await doPetEvents()
+        toc = time.perf_counter()
+        print(f"Pet events complete in {toc - tic:0.4f} seconds")
+        tic = time.perf_counter()
         await doPlayerFixup()
+        toc = time.perf_counter()
+        print(f"Player fixup complete in {toc - tic:0.4f} seconds")
         await asyncio.sleep(3)
     await bot.close()
 
